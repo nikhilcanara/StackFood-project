0418d51c5f65faea3d78f238fb5436f67e35b3fd
nikhilcanara <canaratecnikhil@gmail.com>
2023-09-05 18:16:19 +0530

    implimenting subscribe section

--- src/components/SubscribeSection/index.jsx
+++ src/components/SubscribeSection/index.jsx
@@ -12,15 +12,17 @@ function SubscribeSection() {
                         <img className="left_image" src={LeftImage} alt="" />
                     </div>
                     <div className="section_title_container">
-                        <h1 className="section_title">Lets Connect !</h1>
-                        <h3 className="section_subtitle">Stay Upto Date With Restaurants Around You. Subscribe With Email.</h3>
+                        <h3 className="section_title">Lets Connect !</h3>
+                        <h5 className="section_subtitle">Stay Upto Date With Restaurants Around You. Subscribe With Email.</h5>
                     </div>
                     <div className="section_input_container">
-                        <input placeholder="Your Email Address" type="email" class="section_search_box" value=""></input>
-                        <button className="section_input_button">
-                            <svg class="section_button_icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="KeyboardArrowRightIcon">
-                                <path d="M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"></path></svg>
-                        </button>
+                        <form className="section-form">
+                            <input placeholder="Your Email Address" type="email" className="section_search_box" required />
+                            <button type="submit" className="section_input_button">
+                                <svg className="section_button_icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="KeyboardArrowRightIcon">
+                                    <path d="M8.59 16.59 13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"></path></svg>
+                            </button>
+                        </form>
                     </div>
                     <div className="section_right_image_container">
                         <img className="right_image" src={RightImage} alt="" />
