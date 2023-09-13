49ed5b3d8f67db72c5335badf5cd23b0bb7280d0
nikhilcanara <canaratecnikhil@gmail.com>
2023-09-13 11:01:11 +0530

    added files to Discount banner component

--- src/components/DiscountBanner/index.jsx
+++ src/components/DiscountBanner/index.jsx
@@ -1,11 +1,11 @@
 import react from "react";
 import './index.css';
-import BannerImage from './images/bannerImage.png';
-function DiscountBanner() {
+
+function DiscountBanner(props) {
     return (
         <div className="banner-container">
             <div className="banner_image_container">
-                <img className="banner_image" src={BannerImage} alt="" />
+                <img className="banner_image" src={props.BannerImage} alt="" />
             </div>
         </div>
     );
